04cd828588cf6fdd08780eb74d630810bb4d03a9
kulklex <kulklex@gmail.com>
2022-10-02 18:33:24 +0100

    creating store and adding Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,24 @@
+import { configureStore } from '@reduxjs/toolkit';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
+import {Provider} from "react-redux"
 import App from './App'
 import './index.css'
+import cartReducer from '../cartSlice';
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <Provider store={store}>
   <BrowserRouter>
     <App />
     </BrowserRouter>
+  </Provider>
   </React.StrictMode>
 )

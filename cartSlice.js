6318b031268d180a89aeb6fc253a1a32455fefb5
kulklex <kulklex@gmail.com>
2022-10-02 19:58:13 +0100

    implementing toastify

--- cartSlice.js
+++ cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
 const initialState = {
     items: [],
@@ -14,9 +15,16 @@ const cartSlice = createSlice({
             const itemIndex = state.items.findIndex((item) => item.id === action.payload.id)
             if (itemIndex >= 0) {
                 state.items[itemIndex].cartQty += 1
+                toast.info(`Added Another ${action.payload.title} To Cart`, {
+                    position: "bottom-left",
+                    theme: "colored"
+                })
             } else {
                 const tempProduct = {...action.payload, cartQty: 1};
                 state.items.push(tempProduct);
+                toast.success(`Added ${action.payload.title} To Cart`, {
+                    position: "bottom-left"
+                })
             }
         }
     }

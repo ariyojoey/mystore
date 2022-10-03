325e5ef8ae5f2bdd55766880aec742f667a0a9dc
kulklex <kulklex@gmail.com>
2022-10-03 10:03:14 +0100

    refactoring

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -61,7 +61,7 @@ function Cart() {
       <div>
       {
         cart.items.length === 0 ? (<div className="text-center justify-center items-center">
-          <p className="text-center flex justify-center items-center m-6">Your cart is empty</p>
+          <p className="text-center flex justify-center items-center m-2 md:m-6 overflow-hidden">Your cart is empty</p>
           <hr/>
           <button
             type="button"
@@ -75,16 +75,16 @@ function Cart() {
         : <div className="cartItems">
           {cart.items?.map((item) => (<div key={item.id} className="cartItem">
               <div className="cartProduct flex items-center justify-between m-4">
-                <div className="cartLeft flex items-center  mx-2">
+                <div className="container flex items-center  mx-8">
                   <img width={80} height={50} src={item.image} alt={item.title} />
-                  <h3 className="mx-4 font-bold">{item.title}</h3>
+                  <h3 className=" mx-2 font-bold">{item.title}</h3>
                 </div>
-                <div className="cartCenter flex items-center justify-center">
+                <div className=" hidden md:container md:flex ">
                   <button onClick={() => handleDecrease(item)}>-</button>
-                  <p className="mx-4">{item.cartQty}</p>
+                  <p className="mx-4 px-4">{item.cartQty}</p>
                   <button onClick={() => handleIncrease(item)}>+</button>
                 </div>
-                <div className="cartRight flex mx-4">
+                <div className="flex  md:mx-4">
                   <svg onClick={() => {handleDelete(item)}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-6 cursor-pointer hover:text-red-600">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                   </svg>

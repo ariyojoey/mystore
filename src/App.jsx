61928c993a20fa607e98bf2c02fe0a31ef6b0719
RealRiga <116930426+RealRiga@users.noreply.github.com>
2024-05-24 08:32:53 +0100

    completed the signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import './App.css'
-import 'react-toastify/dist/ReactToastify.css'
-import HomePage from './Pages/homepage'
-import Products from './Pages/Products'
-import Cart from './Pages/Cart'
-import Checkout from './Pages/Checkout';
-import { ToastContainer } from 'react-toastify';
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import Cart from "./Pages/Cart";
+import Checkout from "./Pages/Checkout";
+import Products from "./Pages/Products";
+import HomePage from "./Pages/homepage";
+import LoginPage from "./Pages/login";
+import SignupPage from "./Pages/signup";
 
 function App() {
- 
   return (
     <div className="App">
-      <ToastContainer/>
+      <ToastContainer />
       <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/products/:id" element={<Products/>} />
-          <Route path="/cart" element={<Cart/>} />
-          <Route path="/checkout" element={<Checkout/>} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/products/:id" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,32 +1,48 @@
-import React, {useEffect} from 'react'
-import headerLogo from '../assets/mystore-logo.svg'
-import shopCart from '../assets/shopping-cart.svg'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux';
-import { getTotals } from '../../cartSlice';
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getTotals } from "../../cartSlice";
+import headerLogo from "../assets/mystore-logo.svg";
+import shopCart from "../assets/shopping-cart.svg";
 
 function Header() {
-  const history = useNavigate()
-  const dispatch = useDispatch()
+  const history = useNavigate();
+  const dispatch = useDispatch();
 
-  const cart = useSelector(state => state.cart)
+  const cart = useSelector((state) => state.cart);
   useEffect(() => {
-    dispatch(getTotals())
-  }, [cart, dispatch])
-  
+    dispatch(getTotals());
+  }, [cart, dispatch]);
+
   return (
     <div className="flex items-center py-2 border border-t-0 border-b-2 w-screen px-10 md:px-7 lg:px-12 mt-2  sticky top-0 left-0 right-0 bg-white">
-      
-    <div className='flex-1 flex justify-center'>
-         <img src={headerLogo} className="hover:cursor-pointer" onClick={() => history('/')}/>
-    </div>
-      <img src={shopCart} className="hover:cursor-pointer" onClick={() => history('/cart')} />
-      <span className='flex items-center justify-center rounded-xl bg-blue-500 text-white ml-1 h-5 w-5 text-xs font-bold absolute right-8 md:right-4 lg:right-10 top-2'>
+      <div className="flex-1 flex justify-center">
+        <img
+          src={headerLogo}
+          className="hover:cursor-pointer"
+          onClick={() => history("/")}
+          alt="Logo"
+        />
+      </div>
+      {/* <div className="flex items-center">
+        <Link to="/login" className="text-gray-700 hover:underline mr-4">
+          Login
+        </Link>
+        <Link to="/signup" className="text-gray-700 hover:underline">
+          SignUp
+        </Link>
+      </div> */}
+      <img
+        src={shopCart}
+        className="hover:cursor-pointer ml-4"
+        onClick={() => history("/cart")}
+        alt="Shopping Cart"
+      />
+      <span className="flex items-center justify-center rounded-xl bg-blue-500 text-white ml-1 h-5 w-5 text-xs font-bold absolute right-8 md:right-4 lg:right-10 top-2">
         <span>{cart?.qty}</span>
       </span>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;

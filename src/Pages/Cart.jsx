70c21e7ddcb5f33968affca5f0576f648aa1d127
kulklex <kulklex@gmail.com>
2024-05-29 17:58:19 +0100

    Improved checkout styling

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -54,7 +54,6 @@ function Cart() {
     })
   }
 
-  console.log(cart)
 
   return (
     <div>
@@ -75,19 +74,19 @@ function Cart() {
         Return  Home
       </button>
         </div>) 
-        : <div className="cartItems">
-          {cart.items?.map((item) => (<div key={item._id} className="cartItem">
-              <div className="cartProduct flex items-center justify-between m-4">
-                <div className="container flex items-center  mx-8">
+        : <div className="w-full mx-4">
+          {cart.items?.map((item) => (<div key={item._id}>
+              <div className="flex justify-between m-4">
+                <div className="flex w-1/2 md:w-1/3 items-center">
                   <img width={80} height={50} src={baseUrl+'/uploads/'+item.image} alt={item.title} />
-                  <h3 className=" mx-2 font-bold">{item.title}</h3>
+                  <h3 className="mx-2 font-bold">{item.title}</h3>
                 </div>
-                <div className=" hidden md:container md:flex ">
+                <div className="hidden md:w-1/3 md:flex items-center justify-center text-center">
                   <button onClick={() => handleDecrease(item)}>-</button>
                   <p className="mx-4 px-4">{item.cartQty}</p>
                   <button onClick={() => handleIncrease(item)}>+</button>
                 </div>
-                <div className="flex  md:mx-4">
+                <div className="flex w-1/2 md:w-1/3 items-center">
                   <svg onClick={() => {handleDelete(item)}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-6 cursor-pointer hover:text-red-600">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                   </svg>
@@ -97,11 +96,11 @@ function Cart() {
               <hr/>
           </div>))}
           <hr/>
-          <div className="m-8 flex justify-between">
+          <div className="m-10 flex justify-between">
             <button onClick={confirmClearCart} className="font-bold bg-red-500 text-white rounded-lg p-2 hover:text-red-300">
               Clear Cart
             </button>
-            <div className="cartTotal font-extrabold text-xl">
+            <div className="font-extrabold text-xl">
               Total: {" "} ${Math.trunc(cart.totalAmount * 100) / 100}
             </div>
           </div>

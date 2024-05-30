e03374ef583adb9fe35416f6a265251ee9bc0f4e
kulklex <kulklex@gmail.com>
2024-05-30 05:30:44 +0100

    Refetching single product impl

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import Header from '../components/header'
-import Footer from '../components/Footer'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import {clearCart as clear} from '../redux/cartSlice.js'
-import { baseUrl } from '../main';
-import { toast } from 'react-toastify'
+import React from "react";
+import Header from "../components/header";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart as clear } from "../redux/cartSlice.js";
+import { baseUrl } from "../main";
+import { toast } from "react-toastify";
 
 function Checkout() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const cart = useSelector(state => state.cart)
-
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
 
   let clearCart = () => {
     toast.success(`Thanks for shopping with us`, {
@@ -20,80 +18,79 @@ function Checkout() {
       theme: "colored",
       closeOnClick: true,
       pauseOnHover: true,
-   })
-    dispatch(clear())
-  }
-
+    });
+    dispatch(clear());
+  };
 
   return (
     <div>
-    <Header />
-    <h1 className='text-center m-3'><strong> Checkout </strong></h1>
-    <hr />
+      <Header />
+      <h1 className="text-center m-3">
+        <strong> Checkout </strong>
+      </h1>
+      <hr />
 
-    
-<div className="overflow-x-auto relative">
-
-<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-    <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
-        <tr>
-            <th scope="col" className="py-3 px-6 rounded-l-lg">
-            Product image
-            </th>
-            <th scope="col" className="py-3 px-6 rounded-l-lg">
-                Product name
-            </th>
-            <th scope="col" className="py-3 px-6">
+      <div className="overflow-x-auto relative">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400 pb-4">
+            <tr>
+              <th scope="col" className="py-8 md:py-3 px-6">
+                Image
+              </th>
+              <th scope="col" className=" hidden md:block py-8 md:py-3 px-6">
+                Product
+              </th>
+              <th scope="col" className="py-8 md:py-3 px-6">
                 Qty
-            </th>
-            <th scope="col" className="py-3 px-6 rounded-r-lg">
+              </th>
+              <th scope="col" className="py-8 md:py-3 px-6">
                 Price
-            </th>
-        </tr>
-    </thead>
-    <tbody>
-        {cart.items?.map((item) => (<tr key={item._id} className="bg-white dark:bg-gray-800">
-        <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-            <img src={baseUrl+'/uploads/'+item.image} height={80} width={50} />
-        </th>
-        <td className="py-4 px-6">
-            {item.title}
-        </td>
-        <td className="py-4 px-6">
-            {item.cartQty}
-        </td>
-        <td className="py-4 px-6">
-            ${item.price}
-        </td>
-    </tr>))}
-    </tbody>
-    <tfoot>
-        <tr className="font-semibold text-gray-900 dark:text-white">
-            <th scope="row" className="py-3 px-6 text-base"></th>
-            <th scope="row" className="py-3 px-6 text-base"></th>
-            <td className="py-3 px-6"></td>
-            <td className="py-3 px-6"></td>
-        </tr>
-    </tfoot>
-</table>
-</div>
-{/* <div className='flex justify-center items-center p-4 m-4'>
-    <p className=''>
-        Total: {" "} ${Math.trunc(cart.totalAmount * 100) / 100}
-    </p>
-    <button type="button" className="text-white bg-black hover:bg-black-800 focus:ring-4 focus:ring-black-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-black-600 dark:hover:bg-black-700 focus:outline-none mx-6 dark:focus:ring-black-800" 
-    onClick={() => {navigate('/pay')}}> 
-        Pay 
-    </button>
-</div> */}
-<button type="button" className="text-white bg-black hover:bg-black-800 focus:ring-4 focus:ring-black-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-black-600 dark:hover:bg-black-700 focus:outline-none mx-9 dark:focus:ring-black-800" 
- onClick={() => {clearCart(); navigate('/')}}> 
-    Thanks 
-</button>
-<Footer />
-      
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.items?.map((item) => (
+              <tr key={item._id} className="bg-white dark:bg-gray-800">
+                <th
+                  scope="row"
+                  className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  <img
+                    src={baseUrl + "/uploads/" + item.image}
+                    height={80}
+                    width={50}
+                  />
+                </th>
+                <td className="hidden md:block py-4 px-6">{item.title}</td>
+                <td className="py-4 px-6">{item.cartQty}</td>
+                <td className="py-4 px-6">${item.price}</td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr className="font-semibold text-gray-900 dark:text-white">
+              <th scope="row" className="py-3 px-6 text-base"></th>
+              <th scope="row" className="hidden md:block py-3 px-6 text-base"></th>
+              <td className="py-3 px-6"></td>
+              <td className="py-3 px-6"></td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+      <div className="flex justify-center items-center p-4 m-4">
+        <button
+          type="button"
+          className="text-white bg-black hover:bg-black-800 focus:ring-4 focus:ring-black-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-black-600 dark:hover:bg-black-700 focus:outline-none mx-9 dark:focus:ring-black-800"
+          onClick={() => {
+            clearCart();
+            navigate("/");
+          }}
+        >
+          Proceed to payment
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Checkout
+export default Checkout;
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,82 +1,169 @@
-import React, { useEffect, useState } from 'react'
-import Header from '../components/header'
-import Footer from '../components/Footer'
+import React, { useEffect, useState } from "react";
+import Header from "../components/header";
+import Footer from "../components/Footer";
 import { useParams, useNavigate } from "react-router-dom";
-import Card  from '../components/Card'
-import { useDispatch, useSelector } from 'react-redux';
-import { addToCart } from '../redux/cartSlice';
-import { baseUrl } from '../main';
+import Card from "../components/Card";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../redux/cartSlice";
+import { baseUrl } from "../main";
+import { fetchProducts } from "../redux/productSlice";
 
+function Products() {
+  const { products } = useSelector((state) => state.products);
+  const navigate = useNavigate();
+  let params = useParams();
+  const [value, setValue] = useState(null);
+  const [map, setMap] = useState(null);
+  const dispatch = useDispatch();
 
- function Products() {
-        const { products } = useSelector((state) => state.products);
-        const history = useNavigate();
-        let params = useParams();
-        const [value, setValue] = useState(null)
-        const [map, setMap] = useState(null)
+  const user = localStorage.getItem("userToken");
 
-        const user = localStorage.getItem('userToken')
+  useEffect(() => {
+    // Fetch products if they are not already loaded
+    if (products.length === 0) {
+      dispatch(fetchProducts());
+    }
+  }, [dispatch, products.length]);
 
-        useEffect(() => {
-            setValue(products.filter((x) => `${x._id}` == params.id)[0])
-            setMap(products.filter((x) => `${x._id}` != params.id).slice(0, 3).map((e) => {
-                return <Card title={e.title} price={e.price} image={e.image} key={e.title} onClick={() => history(`/products/${e._id}`, {
+  useEffect(() => {
+    if (products.length > 0) {
+      setValue(products.filter((x) => `${x._id}` == params.id)[0]);
+      setMap(
+        products
+          .filter((x) => `${x._id}` != params.id)
+          .slice(0, 3)
+          .map((e) => {
+            return (
+              <Card
+                title={e.title}
+                price={e.price}
+                image={e.image}
+                key={e.title}
+                onClick={() =>
+                  navigate(`/products/${e._id}`, {
                     replace: true,
-                    state: {}
-                })}/>
-                }))}, [params.id])
-           
-    const dispatch = useDispatch()
-    const handleAddToCart = (value) => {
-        dispatch(addToCart(value));
+                    state: {},
+                  })
+                }
+              />
+            );
+          })
+      );
     }
+  }, [params.id, products, navigate]);
+  console.log(products);
 
-    return (
-        <div className='min-h-screen'>
-        <Header />
-        {
-            value && (
-                <div className="bg-white dark:bg-gray-800 dark:border-gray-700 mt-14 lg:px-16 md:px-8 sm:px-4 w-screen">
-                <div className='flex flex-col-reverse md:flex-row max-w-screen'>
-                    <div className="space-y-5 flex-1 px-10">
-                        <div className='flex items-center space-x-4'>
-                        <a href="#">
-                            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white ">{ value.title }</h5>
-                        </a>
-                        <div className="flex items-center space-x-0">
-                            <svg aria-hidden="true" className="w-5 h-5 text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                            <svg aria-hidden="true" className="w-5 h-5 text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                            <svg aria-hidden="true" className="w-5 h-5 text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                            <svg aria-hidden="true" className="w-5 h-5 text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                            <svg aria-hidden="true" className="w-5 h-5 text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                        </div>
-                        </div>
-                        <div className="flex justify-between items-center">
-                            <span className="text-xs font-regular text-gray-800 font-bold dark:text-white">${value.price}</span>
-                        </div>
-                        <p> {value.description} </p>
-                        {user && <button type="button" className="text-white bg-black hover:bg-black focus:ring-4 focus:ring-black-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-black-600 dark:hover:bg-black-700 focus:outline-none dark:focus:ring-black-800"
-                         onClick={() => handleAddToCart(value)} disabled={!user}>Add to Cart</button>}
-                         {!user && <span className='pb-8 text-base text-gray-400 italic'>Login to add to cart</span>}
-                    </div>
-                    <div className='flex-1'>
-                        <img src={baseUrl+'/uploads/'+value.image} style={{objectFit: 'contain'}}/>
-                    </div>
+  const handleAddToCart = (value) => {
+    dispatch(addToCart(value));
+  };
+
+  return (
+    <div className="min-h-screen">
+      <Header />
+      {value && (
+        <div className="bg-white dark:bg-gray-800 dark:border-gray-700 mt-14 px-4 md:px-8 lg:px-16">
+          <div className="flex flex-col-reverse md:flex-row max-w-screen">
+            <div className="space-y-5 flex-1 p-8">
+              <div className="flex items-center space-x-4">
+                <a href="#">
+                  <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                    {value.title}
+                  </h5>
+                </a>
+                <div className="flex items-center space-x-0">
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5 text-blue-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>First star</title>
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5 text-blue-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Second star</title>
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5 text-blue-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Third star</title>
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5 text-blue-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Fourth star</title>
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5 text-blue-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Fifth star</title>
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
                 </div>
-                
+              </div>
+              <div className="flex justify-between items-center">
+                <span className="text-xs font-regular text-gray-800 font-bold dark:text-white">
+                  ${value.price}
+                </span>
+              </div>
+              <p> {value.description} </p>
+              {user && (
+                <button
+                  type="button"
+                  className="text-white bg-black hover:bg-black focus:ring-4 focus:ring-black-300 font-medium rounded-lg text-sm p-4 mr-2 mb-2 dark:bg-black-600 dark:hover:bg-black-700 focus:outline-none dark:focus:ring-black-800"
+                  onClick={() => handleAddToCart(value)}
+                  disabled={!user}
+                >
+                  Add to Cart
+                </button>
+              )}
+              {!user && (
+                <span className="pb-8 text-base text-gray-400 italic">
+                  Login to add to cart
+                </span>
+              )}
             </div>
-            )
-        }
-    <div className="grid place-items-center w-screen px-4 md:px-8 lg:px-16">
-                        <h3 className='mt-3 text-center'>Recommendations</h3>
-                    </div>
-                    <div className='grid gap-4 px-6 md:grid-cols-2 lg:grid-cols-3 mt-5 w-screen'>
-                        {map}
-                    </div>
+            <div className="flex-1">
+              <img
+                src={baseUrl + "/uploads/" + value.image}
+                style={{ objectFit: "contain" }}
+              />
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="grid place-items-center mt-10 px-4 md:px-8 lg:px-16">
+        <h3 className="mt-3 text-center">Recommendations</h3>
+      </div>
+      <div className="grid gap-4 px-6 md:grid-cols-2 lg:grid-cols-3 mt-5">
+        {map}
+      </div>
 
-        <Footer />
+      <Footer />
     </div>
-    )
-    }
+  );
+}
 
-    export default Products
+export default Products;
